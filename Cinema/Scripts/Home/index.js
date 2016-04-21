fd17c9be48848af69d145c4699e8f904e5f183bd
RHebel1 <Robert.Hebel1@outlook.com>
2016-04-21 12:40:33 +0200

    Updated home page

---
+++ Cinema/Scripts/Home/index.js
@@ -0,0 +1,23 @@
+﻿function parallax() {
+    var scrolled = $(window).scrollTop();
+    $('.bg').css('height', (350 - scrolled) + 'px');
+}
+
+$(window).scroll(function (e) {
+    parallax();
+});
+
+function equalHeight(group) {
+    tallest = 0;
+    group.each(function () {
+        thisHeight = $(this).height();
+        if (thisHeight > tallest) {
+            tallest = thisHeight;
+        }
+    });
+    group.each(function () { $(this).height(tallest); });
+}
+
+$(window).load(function () {
+    equalHeight($(".thumbnail"));
+});
