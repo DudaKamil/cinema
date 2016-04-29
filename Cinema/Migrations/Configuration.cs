b5b027490d124365749f95bebd5a3f206c960d7b
kduda <kamilduda01@gmail.com>
2016-04-29 13:23:58 +0200

    Dodano szyfrowanie haseł użytkowniach przy seedzie bazy danych

--- Cinema/Migrations/Configuration.cs
+++ Cinema/Migrations/Configuration.cs
@@ -2,11 +2,14 @@ using System.Collections.Generic;
 using System.Data.Entity.Migrations;
 using Cinema.DAL;
 using Cinema.Models;
+using Cinema.Services;
 
 namespace Cinema.Migrations
 {
     internal sealed class Configuration : DbMigrationsConfiguration<CinemaContext>
     {
+        private UserRepository repository = new UserRepository();
+
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
@@ -16,8 +19,8 @@ namespace Cinema.Migrations
         {
             var users = new List<User>
             {
-                new User {Login = "user", Password = "user", Name = "User_name", Email = "user@exmaple.com"},
-                new User {Login = "admin", Password = "admin", Name = "Admin_name", Email = "admin@exmaple.com"}
+                new User {Login = "user", Password = repository.EncodePassword("user"), Name = "User_name", Email = "user@exmaple.com"},
+                new User {Login = "admin", Password = repository.EncodePassword("admin"), Name = "Admin_name", Email = "admin@exmaple.com"}
             };
 
             users.ForEach(user => context.Users.AddOrUpdate(p => p.Email, user));
