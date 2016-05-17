898b365172d5b32fcbb76b29170e9fd849ffa121
Robertosen <kontakt.pasternak@gmail.com>
2016-05-17 11:31:28 +0200

    Nawet nie próbuje zrozumieć, dlaczego to sie nie scommitowało. :/

---
+++ Cinema/Services/OrderRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Cinema.DAL;
+using Cinema.Models;
+
+namespace Cinema.Services
+{
+    public class OrderRepository
+    {
+        private CinemaContext db = new CinemaContext();
+
+        public List<Order> GetUserOrdersList(int? userId)
+        {
+            return db.Orders.Where(order => order.UserID == userId).ToList();
+        }
+
+        public Order GetOrder(int? orderId)
+        {
+            return db.Orders.FirstOrDefault(order => order.OrderID == orderId);
+        }
+    }
+}
---
+++ Cinema/Services/SeanceRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Cinema.DAL;
+using Cinema.Models;
+
+namespace Cinema.Services
+{
+    public class SeanceRepository
+    {
+        private CinemaContext db = new CinemaContext();
+
+        public List<Seance> GetSeancesList(int? movieId)
+        {
+            return db.Seances.Where(seance => seance.MovieID == movieId).ToList();
+        }
+
+        public Seance GetSeance(int id)
+        {
+            return db.Seances.FirstOrDefault(u => u.SeanceID == id);
+        }
+
+        public string GetSeanceType(int id)
+        {
+            return db.Seances.FirstOrDefault(u => u.SeanceID == id).Type;
+        }
+
+        public int GetMovieId(int id)
+        {
+            return db.Seances.FirstOrDefault(u => u.SeanceID == id).MovieID;
+        }
+
+        public DateTime GetSeanceDate(int id)
+        {
+            return db.Seances.FirstOrDefault(u => u.SeanceID == id).ShowDate;
+        }
+    }
+}
