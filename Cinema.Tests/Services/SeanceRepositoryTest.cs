57da55a65acdd6af5d572b3371fbe0f547983584
kduda <kamilduda01@gmail.com>
2016-05-17 14:30:58 +0200

    Dodano klasy testowe dla serwisów

---
+++ Cinema.Tests/Services/SeanceRepositoryTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Cinema.Tests
+{
+    [TestClass]
+    public class SeanceRepositoryTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
