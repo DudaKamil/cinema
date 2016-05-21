e04c7406b4dcc9fec57e19cfd34ad2437860a779
RHebel1 <Robert.Hebel1@outlook.com>
2016-05-21 10:26:58 +0200

    Poprawiono przycisk zakupu biletu w repertuarze

---
+++ Cinema/Scripts/vendor/site.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function() {
+
+  $(window).scroll(function () {  
+      console.log($(window).scrollTop())
+    if ($(window).scrollTop() > 150) {
+        $('.navbar').addClass('navbar-fixed-top');
+    }
+    if ($(window).scrollTop() < 151) {
+        $('.navbar').removeClass('navbar-fixed-top');
+
+    }
+  });
+});
+
+function parallax() {
+    var scrolled = $(window).scrollTop();
+    $('.bg').css('height', (150 - scrolled) + 'px');
+}
+
+$(window).scroll(function (e) {
+    parallax();
+});
