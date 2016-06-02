913139f5f9a30b4a005e10daae0da309b7006067
fiquss <fiquss@gmail.com>
2016-06-03 00:50:57 +0200

    Wyświetlanie 6 losowych filmów z bazy na stronie głównej

--- Cinema/Controllers/HomeController.cs
+++ Cinema/Controllers/HomeController.cs
@@ -1,17 +1,39 @@
-﻿using System;
+﻿using Cinema.Services;
+using Cinema.Models;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using System.Net;
+using Cinema.DAL;
 
 namespace Cinema.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly MovieRepository _movieRepository = new MovieRepository(new CinemaContext());
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+
+            List<int> repeat = new List<int>();
+            List<Movie> movieList = new List<Movie>();
+            int random;
+            Random r = new Random();
+            for (int i = 1; i <= 7; i++)
+            {
+                random = r.Next(19)+1;
+                if (repeat.Contains(random))
+                {
+                    while (!repeat.Contains(random))
+                    {
+                        random = r.Next(20);
+                    }
+                }
+                Movie move = _movieRepository.GetMovie(random);
+                repeat.Add(random);
+                movieList.Add(move);
+            }
+            return View(movieList);
         }
     }
 }
