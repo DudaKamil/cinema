edcbd0d9c0af85e959d750c343b7736d513bf19a
martakarwacka <martaxkarwacka@gmail.com>
2016-06-02 22:25:41 +0200

    Dodano przykładowe filmy.

--- Cinema/Migrations/Configuration.cs
+++ Cinema/Migrations/Configuration.cs
@@ -43,20 +43,86 @@ namespace Cinema.Migrations
             {
                 new Movie
                 {
-                    Title = "Movie 1 Title",
-                    Genre = "Movie 1 Genre",
-                    Description = "Movie 1 Description",
-                    ImageURL = "https://placehold.it/350x150",
+                    Title = "Maggie's Plan",
+                    Genre = "Komedia",
+                    Description = "Plany Maggie o samotnym macierzyñstwie spe³zaj¹ na niczym, kiedy zakochuje siê w Johnie, niszcz¹c jego ma³¿eñstwo. ",
+                    ImageURL = "http://media1.woopic.com/93/f/200x250/q/85/fd/p/cinemovies%7C1d6%7C305%7C3e69a4522b6c2d5674df4e9fdd/maggie-a-un-plan%7Cmovies-226326-1.jpg",
                     Length = 120
                 },
                 new Movie
                 {
-                    Title = "Movie 2 Title",
-                    Genre = "Movie 2 Genre",
-                    Description = "Movie 2 Description",
-                    ImageURL = "https://placehold.it/350x150",
+                    Title = "Ben-Hur",
+                    Genre = "Historyczny",
+                    Description = "Mylnie oskar¿ony ksi¹¿ê doœwiadcza lat niewoli, by zemœciæ siê na swoim najlepszym przyjacielu, który go zdradzi³.",
+                    ImageURL = "http://www.fullmoviestoday.com/wp-content/uploads/2016/01/Ben-Hur-2016-Full-Movie-Watch-Online.jpg",
                     Length = 120
-                }
+                },
+                new Movie
+                {
+                    Title = "Central Intelligence",
+                    Genre = "Komedia",
+                    Description = "Po odnowieniu kontaktu ze starym znajomym przez Facebooka ksiêgowy zostaje zwerbowany do miêdzynarodowego wywiadu.",
+                    ImageURL = "http://www.fullmoviestoday.com/wp-content/uploads/2016/02/Central-Intelligence-2016-Movie-Watch-Online.jpg",
+                    Length = 120
+                },
+                new Movie
+                {
+                    Title = "The Legend of Tarzan",
+                    Genre = "Przygodowy",
+                    Description = "Tarzan powraca do Kongo, aby stawiæ czo³o kapitanowi Romowi.",
+                    ImageURL = "http://www.fullmoviestoday.com/wp-content/uploads/2016/01/The-Legend-of-Tarzan-2016-Full-Movie-Online.jpg",
+                    Length = 120
+                },
+                new Movie
+                {
+                    Title = "The BFG",
+                    Genre = "Familijny",
+                    Description = "Porywaj¹ca opowieœæ o ma³ej dziewczynce i tajemniczym Olbrzymie, który odkrywa przed ni¹ cuda i sekrety magicznej krainy.",
+                    ImageURL = "http://photos.elcinema.com/123910498/3e2747fc649a0f515cde701d39db413f_123910498_152.jpg?AWSAccessKeyId=AKIAIE6P6O6F64KPS3OQ&Expires=1973203200&Signature=%2FSxxM42u3kcOl%2B%2FKQ1vT%2BW7OEWU%3D",
+                    Length = 120
+                },
+                new Movie
+                {
+                    Title = "Money Monster",
+                    Genre = "Dramat",
+                    Description = "Money Monster to popularny program telewizyjny, którego gwiazda, Lee Gates (George Clooney), zwany guru Wall Street, doradza widzom, jak i w co inwestowaæ pieni¹dze. Kiedy s³uchaj¹cy jego finansowych wskazówek Kyle Budwell (Jack O'Connell) traci wszystkie oszczêdnoœci, terroryzuje ekipê show i na planie, podczas programu na ¿ywo, grozi Lee œmierci¹, jeœli ten nie sprawi, ¿e akcje, w które zainwestowa³, nie pójd¹ do góry w ci¹gu doby. Publicznoœæ telewizyjna zamiera w przera¿eniu, ogl¹dalnoœæ show roœnie w rekordowym tempie. Ile warte jest ¿ycie cz³owieka? ",
+                    ImageURL = "http://erenguldas.com/portfolyo/izmovie/assets/img/coming-soon-1.jpg",
+                    Length = 120
+                },
+                new Movie
+                {
+                    Title = "Neighbors 2: Sorority Rising",
+                    Genre = "Komedia",
+                    Description = "Kiedy do domu obok wprowadza siê stowarzyszenie studentek, jeszcze gorsze od poprzedniego bractwa, Mac i Kelly prosz¹ o pomoc by³ego s¹siada - Teddy'ego.",
+                    ImageURL = "http://photos.elcinema.com/123928009/8ee7cb3de8ecd67b3129a3fda3d151a7_123928009_152.jpg?AWSAccessKeyId=AKIAIE6P6O6F64KPS3OQ&Expires=1973203200&Signature=KKWkeO%2BAYNuCEo%2FkzWYaDl7dJnY%3D",
+                    Length = 120
+                },
+                new Movie
+                {
+                    Title = "The Young Messiah",
+                    Genre = "Biblijny",
+                    Description = "Opowieœæ o niezwyk³ych wydarzeniach w ¿yciu siedmioletniego Jezusa, które ka¿¹ Mu zadaæ sobie pytanie: kim jestem?",
+                    ImageURL = "http://photos.elcinema.com/123886549/8738b72223b55107abfef1d892e5c699_123886549_152.jpg?AWSAccessKeyId=AKIAIE6P6O6F64KPS3OQ&Expires=1973203200&Signature=O8UtJOkb2knhv46Vw0DYqdsBg40%3D",
+                    Length = 120
+                },
+                new Movie
+                {
+                    Title = "The Jungle Book",
+                    Genre = "Przygodowy",
+                    Description = "Wilcza rodzina przygarnia ch³opca, któremu nadaj¹ imiê Mowgli. Gdy ten podrasta, zaprzyjaŸnia siê z czarn¹ panter¹ Bagheer¹ i niedŸwiedziem Baloo. ",
+                    ImageURL = "http://media.santabanta.com/gallery/Hollywood%20Movies/The%20Jungle%20Book/The-Jungle-Book-1-a_th.jpg",
+                    Length = 120
+                },
+                   new Movie
+                {
+                    Title = "The 5th Wave",
+                    Genre = "Thriller",
+                    Description = "Po inwazji kosmitów nastolatka poszukuje zaginionego brata, który prawdopodobnie zosta³ porwany przez obcych ",
+                    ImageURL = "http://media.santabanta.com/gallery/Hollywood%20Movies/The%205th%20Wave/The-5th-Wave-1-a_th.jpg",
+                    Length = 120
+                },
+
+
             };
 
             movies.ForEach(movie => context.Movies.AddOrUpdate(movie));
